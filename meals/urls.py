d4403fb9ecea9137a5c82a84615d816caba582ef
allan <allansimoesadegas11@gmail.com>
2022-11-19 16:08:41 -0300

    Criando as telas Home e Cardápio

--- meals/urls.py
+++ meals/urls.py
@@ -1,10 +1,17 @@
 
 from django.urls import path
 
-from .views import home_view
+from .views import (
+    home_view,
+    cardapio_view,
+    gerenciar_cardapio_view,
+    gerenciar_estoque_view)
 
 app_name = 'meals'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('cardapio/', cardapio_view, name='cardapio'),
+    path('gerenciar_cardapio/', gerenciar_cardapio_view, name ='cardapio'),
+    path('gerenciar_estoque/', gerenciar_estoque_view, name='estoque'),
 ]
--- meals/views.py
+++ meals/views.py
@@ -5,4 +5,17 @@ from django.shortcuts import render
 
 def home_view(request):
     context = {}
-    return render(request, 'meals/home.html', context)
+    return render(request, 'meals/gerenciar_alunos/home.html', context)
+
+def cardapio_view(request):
+    return render(request, 'meals/cardapio/cardapio.html', {})
+
+def gerenciar_cardapio_view(request):
+    return render(request, 'meals/cardapio/gerenciar_cardapio.html', {})
+
+def gerenciar_estoque_view(request):
+    return render(request, 'meals/estoque/gerenciar_estoque.html')
+
+
+
+
