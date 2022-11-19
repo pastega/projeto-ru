f1c49bf004aab9313d4e5a0023000597e230c9ea
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2022-11-19 13:31:09 -0300

    Primeiros templates e views

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request):
+    context = {}
+    return render(request, 'meals/home.html', context)
--- ru/urls.py
+++ ru/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('meals.urls', namespace='meals')),
+
+    path('login', login_view),
+    path('logout', logout_view)
 ]
