ae056afaab73735d03f2888ad3644009beac4536
allan <allansimoesadegas11@gmail.com>
2022-11-21 09:14:32 -0300

    Estudando meios para fazer a listagem e edição de cardápios

--- meals/views.py
+++ meals/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import Refeicao
+from datetime import date, timedelta
 
 # Create your views here.
 
@@ -7,11 +10,21 @@ def home_view(request):
     context = {}
     return render(request, 'meals/gerenciar_alunos/home.html', context)
 
-def cardapio_view(request):
-    return render(request, 'meals/cardapio/cardapio.html', {})
+class CardapioView(ListView):
+    model = Refeicao
+    template_name = 'meals/cardapio/cardapio.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = {'now': date.today(), 'limit':date.today()+timedelta(7)}
+        return context
 
-def gerenciar_cardapio_view(request):
-    return render(request, 'meals/cardapio/gerenciar_cardapio.html', {})
+# def cardapio_view(request):
+#     return render(request, 'meals/cardapio/cardapio.html', {})
+
+class GerenciarCardapioView(ListView):
+    model = Refeicao
+    template_name = 'meals/cardapio/gerenciar_cardapio.html'
+
 
 def gerenciar_estoque_view(request):
     return render(request, 'meals/estoque/gerenciar_estoque.html')
